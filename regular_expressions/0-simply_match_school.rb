b1fba11725a91f9d9a35f075fdab885b2f2c0376
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-09-15 13:52:24 +0200

    Update 0-simply_match_school.rb

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and prints all occurrences of "School" using a regular expression.
-puts ARGV[0].scan(/School/).join
+puts ARGV[0].scan(/hb*t*n/).join
